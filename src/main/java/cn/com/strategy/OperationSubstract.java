b7e7b40a73e0e4e08a567294c366adb5f022253f
chenzheng <chenzhengmsg@163.com>
2019-03-07 17:15:59 +0800

    策略模式

---
+++ src/main/java/cn/com/strategy/OperationSubstract.java
@@ -0,0 +1,10 @@
+package cn.com.strategy;
+
+public class OperationSubstract implements Strategy {
+
+	public int doOperation(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return num1 - num2;
+	}
+
+}
---
+++ src/main/java/cn/com/strategy/Strategy.java
@@ -0,0 +1,11 @@
+package cn.com.strategy;
+
+/**
+ * 策略模式
+ * @author chenzheng
+ *
+ */
+public interface Strategy {
+
+	public int doOperation(int num1,int num2);
+}
